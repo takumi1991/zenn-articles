7d315d376a8d52d7f5bccdfca9055b42c6de7347
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2026-04-15 16:26:31 +0900

    Update links for AWS and Google Cloud free tiers

--- scripts/update-azure.js
+++ scripts/update-azure.js
@@ -119,10 +119,10 @@ AzureにもAWSやGoogle Cloud同様に「常時無料枠（Always Free Services�
 
 ## 関連リンク：AWSやGoogle Cloudの常時無料枠
 
-AWS Free Tier (Always Free)  
+AWS Always Free Tier List
 👉 https://zenn.dev/good_sleeper/articles/aws-always-free
 
-Google Cloud Platform Free Tier (Always Free)  
+Google Cloud Platform Always Free Tier List
 👉 https://zenn.dev/good_sleeper/articles/gcp-always-free
 `;
 
