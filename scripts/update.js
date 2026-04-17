8d077b2c59463526b85c51f0180970a0d53c9a22
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2026-04-17 10:09:16 +0900

    Change line separator in update.js

--- scripts/update.js
+++ scripts/update.js
@@ -220,7 +220,7 @@ const CATEGORY_ORDER = [
       
           // 👇 最後以外だけ区切り線
           if (index !== list.length - 1) {
-            md += `<br>\n\n`;
+            md += `---\n\n`;
           } else {
             // 👇 最後だけn行余白
             md += `<br><br>\n`;
