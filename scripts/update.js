c710ab334e1f972724a09bf251c79f7a0dbfd6c8
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2026-04-18 02:21:57 +0900

    Update links to use arrow for better visibility

--- scripts/update.js
+++ scripts/update.js
@@ -229,10 +229,10 @@ Amazon Web Services の常時無料枠（Always Free Services）はアカウン�
    ## 関連記事：他クラウドの常時無料枠まとめ
    
    🌈 Google Cloud Platform の常時無料枠  
-   https://zenn.dev/good_sleeper/articles/gcp-always-free
+   👉 https://zenn.dev/good_sleeper/articles/gcp-always-free
    
    🟦 Microsoft Azure の常時無料枠 (Always Free Services)  
-   https://zenn.dev/good_sleeper/articles/azure-always-free
+   👉 https://zenn.dev/good_sleeper/articles/azure-always-free
    
    `;
 
